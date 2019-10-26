3296734e0ff7ca33b083b492c91e47fe21b5299c
stophman1 <11683358+stophman1@users.noreply.github.com>
2019-10-26 10:13:54 -0500

    Update link_footer.js

--- app/javascript/flavours/glitch/features/ui/components/link_footer.js
+++ app/javascript/flavours/glitch/features/ui/components/link_footer.js
@@ -61,7 +61,7 @@ class LinkFooter extends React.PureComponent {
             defaultMessage='Glitchsoc is open source software, a friendly fork of {Mastodon}. You can contribute or report issues on GitHub at {github}. Our fork is available at {blobhub}'
             values={{
               github: <span><a href='https://github.com/glitch-soc/mastodon' rel='noopener' target='_blank'>glitch-soc/mastodon</a> (v{version})</span>,
-              blobhub: <a href='https://github.com/blobturtle-club/mastodon' rel='noopener' target='_blank'>here</a> }},
+              blobhub: <a href='https://github.com/blobturtle-club/mastodon' rel='noopener' target='_blank'>here</a>,
               Mastodon: <a href='https://github.com/tootsuite/mastodon' rel='noopener' target='_blank'>Mastodon</a> }}
           />
         </p>
